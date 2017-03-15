9d54173ce3d69e2649952a6624777e2894a805db
Nicholas Nadeau <nicholas.nadeau.1@ens.etsmtl.ca>
2017-03-15 13:25:39 -0400

    added project start

---
+++ src/main/java/me/nicholasnadeau/robot/comm/Server.java
@@ -0,0 +1,40 @@
+package me.nicholasnadeau.robot.comm;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+
+/**
+ * Created on 2017-03-15.
+ * <p>
+ * Copyright Nicholas Nadeau 2017.
+ */
+public class Server implements Runnable {
+    static int port = 30000;
+
+    public Server(int port) {
+        Server.port = port;
+    }
+
+    @Override
+    public void run() {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioDatagramChannel.class)
+                    .option(ChannelOption.SO_BROADCAST, true)
+                    .handler(new ServerHandler());
+
+
+
+            b.bind(port).sync().channel().closeFuture().await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/me/nicholasnadeau/robot/comm/ServerHandler.java
@@ -0,0 +1,12 @@
+package me.nicholasnadeau.robot.comm;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandler;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.util.CharsetUtil;
+
+public class ServerHandler extends SimpleChannelInboundHandler {
+
+}
